8c475beb669e83b9cadc53f3c482275133170e8b
roman-kenda <sir.kenda@yandex.ru>
2016-07-05 13:38:06 +0300

    Add devise and params

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {registration: 'users/sign_up'}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
