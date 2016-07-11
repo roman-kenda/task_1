13ada4734ef58ab2348b32710141480e39b3d5aa
roman-kenda <sir.kenda@yandex.ru>
2016-07-11 12:58:34 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,19 @@ Rails.application.routes.draw do
   get 'pages/dashboard', as: "dashboard"
 
   devise_for :users, controllers:  { registrations: 'users/registrations' }
+  devise_scope :user do
+  resource :registration,
+    only: [:new, :create],
+    path: 'users',
+    path_names: { new: 'sign_up' },
+    controller: 'devise/registrations',
+    as: :user_registration do
+      get :cancel
+    end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
 
   # You can have the root of your site routed with "root"
   root 'pages#dashboard'
