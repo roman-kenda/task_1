68fedb69124f8096a21af7adc836dcb8c6df88f7
roman-kenda <sir.kenda@yandex.ru>
2016-07-13 01:33:22 +0300

    add and correct params of images

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,2 +1,22 @@
 module PicturesHelper
+
+  def human_size(size)
+    (size/100000).round(2)
+  end
+
+  def  extension(image_url)
+    only_url(image_url)[0].reverse
+  end
+
+  def name(image_url)
+    only_url(image_url)[1].split('/')[0].reverse
+  end
+
+  private
+
+  def only_url(image_url)
+  	url = image_url.reverse
+    url.split('.', 2)
+  end
+
 end
