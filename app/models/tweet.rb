457a17bc5cc77d8ec0072b0f476481ed785fa0cd
roman-kenda <sir.kenda@yandex.ru>
2016-07-25 11:43:31 +0300

    sending tweets to twitter

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,8 @@
 class Tweet < ActiveRecord::Base
   belongs_to :twitter_account
+  before_create :post_to_twitter
+
+  def post_to_twitter
+    twitter_account.twitter.update(body)
+  end
 end
--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -3,4 +3,13 @@ class TwitterAccount < ActiveRecord::Base
   has_many :tweets, dependent: :destroy
   
   validates :uid, uniqueness: true
+
+  def twitter
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.twitter_api_key
+      config.consumer_secret     = Rails.application.secrets.twitter_api_secret
+      config.access_token        = token
+      config.access_token_secret = secret
+    end
+  end
 end
